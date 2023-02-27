b6d6c5e6fb945836c901755b735ea4a48990276e
Mikkisbusta <djepse20@student.aau.dk>
2023-02-27 11:07:58 +0100

    here you go

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ fn main() {
     let vec1 = vec!['a','b','c','d','e'];
     let vec2 =  vec!['x','x','c','x','x'];
     let allignment : Vec<usize> = vec![2];
+
+    
     let(parts1, parts2) = vec1.into_iter().zip(vec2).enumerate().fold(
         (Vec::new(),Vec::new()), |(mut parts1,mut parts2),(idx, parts)| {
         if idx == 0 || allignment.contains(&idx) {
             parts1.push(Vec::new());
             parts2.push(Vec::new());
-        } else {
+        } 
+        if  !allignment.contains(&idx) {
             parts1.last_mut().unwrap().push(parts.0);
             parts2.last_mut().unwrap().push(parts.1);
 
@@ -15,4 +18,5 @@ fn main() {
         (parts1,parts2)
 
     });
+    println!("{:?}{:?}",parts1,parts2);
 }
